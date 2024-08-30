97dcb3446ea6a0229db053a8f94ff681d030ffd9
Yoana <116558529+yoanadivanova@users.noreply.github.com>
2024-08-30 15:13:06 +0200

    noise

---
+++ experiments/noise2.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(1000, 1000);
+  frameRate(10);
+}
+
+const size = 5;
+const divider = 5;
+const numRows = 200;
+const numCols = 200;
+
+let counter = 0;
+
+function draw() {
+  background(0);
+  noStroke();
+  fill(255, 0, 0);
+
+  // noiseSeed(0);
+  for (let y = 0; y < numRows; y++) {
+    for (let x = 0; x < numCols; x++) {
+      const value = noise(x / divider, y / divider, counter) * size;
+      ellipse(size / 2 + x * size, size / 2 + y * size, value);
+    }
+  }
+
+  counter += 1;
+
+  //noLoop();
+}
